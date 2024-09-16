75a186847cc433e6864e160369164633aa1e8390
Anastasia Chistyakova <kozlova27as@gmail.com>
2024-09-17 00:10:50 +0300

    test commit

--- src/test/java/ru/netology/AuthTest.java
+++ src/test/java/ru/netology/AuthTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
